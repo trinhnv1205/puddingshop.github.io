f56cf744320fd094a26477e91c06854b716e511c
trinhnv <trinhnv@gemvietnam.com>
2024-01-14 13:24:15 +0700

    new file:   swagger.js new file:   swagger_output.json new file:   ../package-lock.json new file:   ../package.json

---
+++ backend/swagger.js
@@ -0,0 +1,7 @@
+// swagger.js
+const swaggerAutogen = require('swagger-autogen')()
+
+const outputFile = './swagger_output.json'
+const endpointsFiles = ['./app.js']
+
+swaggerAutogen(outputFile, endpointsFiles)
