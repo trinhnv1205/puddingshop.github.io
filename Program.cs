bfac0a846cce5c4d30ec5ef6263c70d21777d450
trinhnv <blackwizard1205@gmail.com>
2024-08-10 16:41:12 +0700

    restore non-authen

--- Program.cs
+++ Program.cs
@@ -11,20 +11,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddFluentUIComponents();
 
-builder.Services.AddOidcAuthentication(options =>
-{
-    // Configure your authentication provider options here.
-    // For more information, see https://aka.ms/blazor-standalone-auth
-    builder.Configuration.Bind("Local", options.ProviderOptions);
-    
-    // google authentication
-    options.ProviderOptions.Authority = "https://accounts.google.com";
-    options.ProviderOptions.ClientId = "836213548737-04qcbn0j2j5hiu4hqgik1adhflu6d49u.apps.googleusercontent.com";
-    options.ProviderOptions.ResponseType = "id_token";
-    options.ProviderOptions.DefaultScopes.Add("email");
-    options.ProviderOptions.DefaultScopes.Add("profile");
-    options.ProviderOptions.DefaultScopes.Add("openid");
-});
-
-
 await builder.Build().RunAsync();
